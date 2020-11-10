e394816f41184982c87614b355386ed40b504207
miele.riccardo <miele.riccardo@TAVE-PC28.OSDBSESTO.local>
2020-11-10 13:02:59 +0100

    add unless loop

---
+++ src/provagit/Provagit.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provagit;
+
+/**
+ *
+ * @author miele.riccardo
+ */
+public class Provagit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    int i = 0;
+    while (true){
+    System.out.println("ehi ciao: "+ i);
+    i++;
+}
+    }
+    
+}
